d8e39669977a86c582e17d90abfccc56cf643b2d
bgrochal <bartek.grochal@gmail.com>
2017-11-30 23:37:47 +0100

    Integration with the Configuration Server.

--- src/main/java/pl/edu/agh/iet/dts/ui/UIService.java
+++ src/main/java/pl/edu/agh/iet/dts/ui/UIService.java
@@ -1,12 +1,16 @@
 package pl.edu.agh.iet.dts.ui;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Bartłomiej Grochal
  */
+@SpringBootApplication
 public class UIService {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(UIService.class, args);
     }
 
 }
