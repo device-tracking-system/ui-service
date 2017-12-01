55944ee4393af2da949f11c5d6ca2a1c424ceac8
bgrochal <bartek.grochal@gmail.com>
2017-12-01 17:46:32 +0100

    Integration with the Eureka service discovery.

--- src/main/java/pl/edu/agh/iet/dts/ui/UIService.java
+++ src/main/java/pl/edu/agh/iet/dts/ui/UIService.java
@@ -2,11 +2,13 @@ package pl.edu.agh.iet.dts.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Bartłomiej Grochal
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UIService {
 
     public static void main(String[] args) {
